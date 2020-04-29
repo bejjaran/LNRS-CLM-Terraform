13c4f4213c156399a49cb3cbd8b1d8d5cfcbf0db
Czire, Robert <robert.czire@lexisnexisrisk.com>
2020-04-29 09:55:58 -0400

    dev subnet tag update

--- modules/clm/data.tf
+++ modules/clm/data.tf
@@ -19,7 +19,8 @@ data "aws_subnet" "subnets" {
 data "aws_subnet_ids" "lb_private" {
   vpc_id = data.aws_vpc.selected.id
   tags = {
-    Name   = "${var.aws_vpc}-private-*"
+    #Name   = "${var.aws_vpc}-private-*"
+    Name   = "${var.aws_vpc} zone-* internal"
   }
 }
 
@@ -27,6 +28,7 @@ data "aws_subnet_ids" "lb_private" {
 data "aws_subnet_ids" "lb_public" {
   vpc_id = data.aws_vpc.selected.id
   tags = {
-    Name   = "${var.aws_vpc}-public-*"
+    #Name   = "${var.aws_vpc}-public-*"
+    Name   = "${var.aws_vpc} zone-* dmz"
   }
 }
