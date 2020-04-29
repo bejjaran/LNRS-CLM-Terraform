b7314c148bbcf194bcad843da75fdb9d83f4a724
Czire, Robert <robert.czire@lexisnexisrisk.com>
2020-04-29 12:46:57 -0400

    replace public in ASG

--- modules/clm/autoscale.tf
+++ modules/clm/autoscale.tf
@@ -31,7 +31,8 @@ resource "aws_autoscaling_group" "web" {
   desired_capacity     = var.asg_min
   launch_configuration = aws_launch_configuration.web.name
   target_group_arns    = ["${aws_lb_target_group.web-443.arn}"]
-  vpc_zone_identifier  = ["${data.aws_subnet.subnets["public"].id}"]
+  #vpc_zone_identifier  = ["${data.aws_subnet.subnets["public"].id}"]
+  vpc_zone_identifier  = ["${data.aws_subnet.subnets["dmz"].id}"]
   termination_policies = ["OldestInstance"]
 
   enabled_metrics = [
