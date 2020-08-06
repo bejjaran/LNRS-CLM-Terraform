63ff07a98b8158896770815db08a63268a56da18
Czire, Robert <you@example.com>
2020-08-06 13:12:25 -0400

    add vars for public & priv subnet

--- modules/clm/variables.tf
+++ modules/clm/variables.tf
@@ -32,6 +32,14 @@ variable "aws_lb_subnets" {
   description = "The subnets you wish to deploy load balancers into (e.g eu-west-1a, eu-west-1b)"
 }
 
+variable "aws_private_subnet" {
+  description = "The private subnet"
+}
+
+variable "aws_public_subnet" {
+  description = "The private subnet"
+}
+
 variable "aws_public_subnet_cidr" {
   type = set(string)
   description = "The public subnet CIDR for LB Egress SG"
