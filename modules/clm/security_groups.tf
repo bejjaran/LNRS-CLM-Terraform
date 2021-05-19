16e90cd7839dd098c40e655fe8f514df88f131ee
czirro01 <robert.czire@lexisnexisrisk.com>
2021-05-19 17:22:45 -0400

    add winrm to web sg

--- modules/clm/security_groups.tf
+++ modules/clm/security_groups.tf
@@ -95,6 +95,14 @@ resource "aws_security_group" "web" {
     description = "Internal - HTTPS Inbound"
   }
 
+  ingress {
+    from_port   = "5986"
+    to_port     = "5986"
+    protocol    = "tcp"
+    cidr_blocks = ["10.0.0.0/8", "172.16.0.0/12", "192.168.0.0/16"]
+    description = "Internal - WinRM-HTTPS Inbound"
+  }
+
   egress {
     from_port   = "80"
     to_port     = "80"
